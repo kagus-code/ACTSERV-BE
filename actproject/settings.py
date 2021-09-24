93b28178fb1c6f28699bb10b8b966f73d89c65ed
Eston Kagwima <ekagwima745@gmail.com>
2021-09-24 20:28:04 +0300

    installing django rest_framework

--- actproject/settings.py
+++ actproject/settings.py
@@ -63,6 +63,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'passwordless.apps.PasswordlessConfig'
+    'rest_framework',
+
+
 ]
 
 MIDDLEWARE = [
@@ -125,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
